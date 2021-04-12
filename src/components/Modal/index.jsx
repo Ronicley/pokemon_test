8bbbadfdc2ffda3a4fbd8fa0e78eca3d17015722
ronicley.sa <ronicley.sa@crptecnologia.com.br>
2021-04-12 20:15:40 -0300

    add modal structure

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as Style from "./styled";
+
+const Modal = () => {
+
+  return (
+    <Style.ModalContent>
+      Text
+    </Style.ModalContent>
+  )
+};
+
+export default Modal
---
+++ src/components/Modal/styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ModalContent = styled.div`
+  background: linear-gradient(90deg, #43E97B 0%, #38F9D7 100%);
+`;
