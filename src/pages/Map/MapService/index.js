ddab5d4f6177f2a83064e1b2c3e2a491d0e57db9
ronicley.sa <ronicley.sa@crptecnologia.com.br>
2021-04-13 20:56:55 -0300

    endpoint to get randomized Pokemon's

---
+++ src/pages/Map/MapService/index.js
@@ -0,0 +1,13 @@
+import axios from "../../../services/axios.wrapper";
+
+const getRndInteger = () => {
+  return Math.floor(Math.random() * (807 - 1 + 1) ) + 1;
+}
+
+export const getRandomicPokemon = async () => {
+  try{
+    return await axios.get(`pokmon/${getRndInteger()}`);
+  }catch (e){
+    console.error(e)
+  }
+}
