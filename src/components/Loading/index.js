8248321b8838ca2a0165c49229e2b9c827b71ee0
ronicley.sa <ronicley.sa@crptecnologia.com.br>
2021-04-17 11:00:26 -0300

    add loading to the pokemon modal

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactLoading from "react-loading";
 
 // eslint-disable-next-line react/prop-types
-const Loading = ({type, color}) => {
-  return <ReactLoading type={type} color={color} height={200} width={100}/>;
+const Loading = ({type = "spin", color = "#ffffff", width = 100, height = 200}) => {
+  return <ReactLoading type={type} color={color} height={height} width={width}/>;
 };
 
 export default Loading;
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,67 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
 import * as Style from "./styled";
 import closeIcon from "../../assets/images/close.png";
 import Button from "../Button";
 import pokeball from "../../assets/images/pokeball.png";
 import Features from "../features";
-import { getRandomicPokemon } from "../../pages/Map/MapService";
+import {getRandomicPokemon} from "../../pages/Map/MapService";
+import Loading from "../Loading";
 
 // eslint-disable-next-line react/prop-types
-const Modal = ({ open = false, onClose }) => {
-    const [pokemon, setPokemon] = useState({});
+const Modal = ({open = false, onClose}) => {
+  const [pokemon, setPokemon] = useState({});
+  const [loading, setLoading] = useState(false);
 
-    const getPokemon = async () => {
-        let { data } = await getRandomicPokemon();
-        setPokemon(data);
-    };
+  const getPokemon = async () => {
+    setLoading(true);
+    try {
+      let {data} = await getRandomicPokemon();
+      setPokemon(data);
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
+  };
 
-    const handleCapturePokemon = () => {
+  const handleCapturePokemon = () => {
 
-    };
+  };
 
-    useEffect(() => {
-        if (open) {
-            getPokemon().then();
+  useEffect(() => {
+    if (open) {
+      getPokemon().then();
+    }
+  }, [open]);
+
+  return (
+    open && (
+      <Style.Container>
+        {
+          loading ? (
+            <Loading/>
+          ) : (
+            <Style.ModalBody>
+              <Style.CloseButton onClick={onClose} src={closeIcon}/>
+              <Style.ModalHeader>
+                {
+                  pokemon?.sprites && (
+                    <img
+                      src={pokemon?.sprites?.front_default}
+                      width={200}
+                      alt="Pokemon Sprite"
+                    />
+                  )
+                }
+              </Style.ModalHeader>
+              <Style.ModalContent>
+                {
+                  pokemon.stats && (
+                    <Features
+                      height={pokemon.height}
+                      hp={pokemon.stats[0].base_stat}
+                      weight={pokemon.weight}
+                      name={pokemon.name}
+                      abilities={pokemon.abilities}
+                      types={pokemon.types}
+                    />
+                  )
+                }
+              </Style.ModalContent>
+              <Style.PokeballContaier>
+                <Button
+                  icon={pokeball}
+                  onClick={handleCapturePokemon}
+                />
+              </Style.PokeballContaier>
+            </Style.ModalBody>
+          )
         }
-    }, [open]);
-
-    return (
-        open && (
-            <Style.Container>
-                <Style.ModalBody>
-                    <Style.CloseButton onClick={onClose} src={closeIcon} />
-                    <Style.ModalHeader>
-                        {pokemon?.sprites && (
-                            <img
-                                src={pokemon?.sprites?.front_default}
-                                width={200}
-                                alt="Pokemon Sprite"
-                            />
-                        )}
-                    </Style.ModalHeader>
-                    <Style.ModalContent>
-                        {pokemon.stats && (
-                            <Features
-                                height={pokemon.height}
-                                hp={pokemon.stats[0].base_stat}
-                                weight={pokemon.weight}
-                                name={pokemon.name}
-                                abilities={pokemon.abilities}
-                                types={pokemon.types}
-                            />
-                        )}
-                    </Style.ModalContent>
-                    <Style.PokeballContaier>
-                        <Button
-                            icon={pokeball}
-                            onClick={handleCapturePokemon}
-                        />
-                    </Style.PokeballContaier>
-                </Style.ModalBody>
-            </Style.Container>
-        )
-    );
+      </Style.Container>
+    )
+  );
 };
 
 export default Modal;
